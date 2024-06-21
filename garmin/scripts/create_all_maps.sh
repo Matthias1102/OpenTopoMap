bd546f1e43f18c7e61d123ea2216ed03e214e841
Matthias1102 <m.doerbecker@gmx.de>
2024-06-21 19:01:55 +0200

    Added script create_all_maps.sh

---
+++ garmin/scripts/create_all_maps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create a Garmin map for the regions mentioned below.
+#
+./create_map.sh https://download.geofabrik.de/europe/germany-latest.osm.pbf 4900
+./create_map.sh https://download.geofabrik.de/europe/united-kingdom-latest.osm.pbf 4400
+./create_map.sh https://download.geofabrik.de/europe/france-latest.osm.pbf 3300
+./create_map.sh https://download.geofabrik.de/europe/switzerland-latest.osm.pbf 4100
+./create_map.sh https://download.geofabrik.de/europe/austria-latest.osm.pbf 4300
+./create_map.sh https://download.geofabrik.de/europe/italy-latest.osm.pbf 3900
+./create_map.sh https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf 4230
+./create_map.sh https://download.geofabrik.de/europe/belgium-latest.osm.pbf 3200
+./create_map.sh https://download.geofabrik.de/europe/netherlands-latest.osm.pbf 3100
+./create_map.sh https://download.geofabrik.de/europe/norway-latest.osm.pbf 4700
+./create_map.sh https://download.geofabrik.de/europe/spain-latest.osm.pbf 3400
+./create_map.sh https://download.geofabrik.de/europe/denmark-latest.osm.pbf 4500
+./create_map.sh https://download.geofabrik.de/europe/sweden-latest.osm.pbf 4600
+./create_map.sh https://download.geofabrik.de/europe/ireland-latest.osm.pbf 3530
+./create_map.sh https://download.geofabrik.de/europe/portugal-latest.osm.pbf 3510
