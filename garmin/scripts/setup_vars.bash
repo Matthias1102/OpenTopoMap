0030efc413388abd4ef8d2c285502c5dd9a8c103
Matthias1102 <m.doerbecker@gmx.de>
2024-06-22 19:03:42 +0200

    Added setup_vars.bash

---
+++ garmin/scripts/setup_vars.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# In order to set the following env variables persistently, this script
+# has to be 'sourced':
+#
+# source ./setup_vars.bash
+#
+export MKGMAP="mkgmap-r4921"
+export SPLITTER="splitter-r654"
