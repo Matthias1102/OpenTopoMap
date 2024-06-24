35901b118554bc6fe807a35751525da5c812d083
Matthias1102 <m.doerbecker@gmx.de>
2024-06-24 19:38:12 +0200

    Updated script cleanup.sh

--- garmin/scripts/cleanup.sh
+++ garmin/scripts/cleanup.sh
@@ -8,6 +8,6 @@
 cd $(dirname $0)/.. || exit 1
 
 echo "Removing these folders: mkgmap_work, splitter_out"
-echo "The following folders will be kept: download_geofabrik, output"
+echo "The following folders will be kept: dem1, download_geofabrik, output"
 rm -rf mkgmap_work
 rm -rf splitter_out
